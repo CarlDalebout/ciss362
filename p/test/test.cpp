624a9bd3c5ff0290c57def79af99174f21a2488d
Carl <cadalebout1@ccis.edu>
2024-12-11 23:26:30 -0600

    adding in test files in p/

--- p/test/test.cpp
+++ p/test/test.cpp
@@ -5,7 +5,8 @@
 #include <unordered_set>
 #include <unordered_map>
 #include "../src/STL_util.h"
-#include "../src/DFA.h"
+
+
 
 int main()
 {
@@ -13,28 +14,42 @@ int main()
   typedef std::string S_t;
   typedef std::pair< Q_t, S_t > Q_t_S_t;
   typedef std::unordered_map< Q_t_S_t, Q_t > D_t;
-  
+  typedef std::unordered_multimap< Q_t_S_t , Q_t> MD_t;
+ 
   S_t a = "a";
   S_t b = "b";
+  S_t e = "epsilon";
+
   Q_t q0 = "q0";
   Q_t q1 = "q1";
   Q_t q2 = "q2";
+  Q_t q3 = "q3";
+  Q_t q4 = "q4";
+  Q_t q5 = "q5";
   
   std::unordered_set< S_t > S {a, b};
+
   std::unordered_set< Q_t > Q {q0, q1, q2};
-  std::unordered_set< Q_t > F {q1};
 
-  D_t delta;
-  delta[{q0, a}] = q0;
-  delta[{q0, b}] = q1;
-  delta[{q1, a}] = q1;
-  delta[{q1, b}] = q2;
-  delta[{q2, a}] = q0;
-  delta[{q2, b}] = q2;
+  std::unordered_set< Q_t > current_states = {q0, q1};  // Start at the initial state
 
-  DFA< S_t, Q_t > M(S, Q, q0, F, delta);
+  std::unordered_set< Q_t > F {q2};
 
-  std::list< S_t > w {a, b, a, a};
-  std::cout << M(w) << '\n'; // M(w) is true if M accepts abaa
+  MD_t delta;
+  delta.insert({{q0, a}, q0});
+  delta.insert({{q0, b}, q0});
+  delta.insert({{q0, b}, q1});
+
+  delta.insert({{q1, a}, q0});
+  delta.insert({{q1, b}, q1});
+
+  delta.insert({{q2, a}, q2});
+  delta.insert({{q2, b}, q2});
+
+  for(auto& x : delta)
+  {
+    std::cout << x << std::endl;
+  }
+
   return 0;
 }
